11f423dbf942926df2a7549ac0a31bf26869815d
BadEnding-Official <bedcoin.official@gmail.com>
2025-04-14 01:41:02 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+// 메뉴 항목 클릭 시 스크롤 애니메이션 추가
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1); // "#id"에서 id만 추출
+        const targetElement = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop - 50, // 네비게이션 바의 높이를 제외하고 스크롤
+            behavior: 'smooth' // 부드러운 스크롤 효과
+        });
+    });
+});
